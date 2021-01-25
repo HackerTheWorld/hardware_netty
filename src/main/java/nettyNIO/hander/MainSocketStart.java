dbf686d34e9102650f2b235afc331d24e9de762e
ppss <wh3164335@163.com>
2021-01-25 21:31:50 +0800

    添加输出

--- src/main/java/nettyNIO/hander/MainSocketStart.java
+++ src/main/java/nettyNIO/hander/MainSocketStart.java
@@ -8,6 +8,8 @@ public class MainSocketStart {
     public static void main(String[] args){
         SocketStart.startSocket(9000);
         WebSocketStart.startWebSocket(9001);
+        System.out.println("socket ::"+9000);
+        System.out.println("webSocket ::"+9001);
     }
 
 }
