69fdc07d0ca0547690f14adb0c84e263a0265416
PanLingYun <wh3164335@163.com>
2021-08-24 14:34:56 +0800

    [fix] channel

--- src/main/java/nettyNIO/hander/socket/SocketInta.java
+++ src/main/java/nettyNIO/hander/socket/SocketInta.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.handler.codec.json.JsonObjectDecoder;
 import nettyNIO.hander.socket.hander.SocketFilterHander;
 import nettyNIO.hander.socket.hander.SocketHanderBackUpToData;
+import nettyNIO.hander.socket.hander.SocketHanderIntoMq;
 
 /**
  * 初始化通道
@@ -14,14 +15,15 @@ public class SocketInta extends ChannelInitializer<Channel> {
     @Override
     protected void initChannel(Channel channel) throws Exception {
         //特殊字符解码器
-//        ByteBuf buf = Unpooled.copiedBuffer("//".getBytes());
-//        channel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,buf));
+        //ByteBuf buf = Unpooled.copiedBuffer("//".getBytes());
+        //channel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,buf));
         // 自定义解码
         // channel.pipeline().addLast(new SocketDecoder());
         // 自定义编码
         //channel.pipeline().addLast(new StringEncoder());
-        channel.pipeline().addLast(new JsonObjectDecoder());
-        channel.pipeline().addLast(new SocketFilterHander());
-        channel.pipeline().addLast(new SocketHanderBackUpToData());
+        channel.pipeline().addFirst("jsonDecoder",new JsonObjectDecoder());
+        channel.pipeline().addAfter("jsonDecoder","filter",new SocketFilterHander());
+        channel.pipeline().addAfter("filter","backUp",new SocketHanderBackUpToData());
+        channel.pipeline().addLast("intoMq",new SocketHanderIntoMq());
     }
 }
